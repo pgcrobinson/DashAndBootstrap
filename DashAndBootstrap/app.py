ba1b7648a9777876dc9e54f0e2c25185bccbf03e
pgcrobinson <pgcrobinson@gmail.com>
2019-01-28 13:45:20 +0000

    one more change

--- DashAndBootstrap/app.py
+++ DashAndBootstrap/app.py
@@ -121,7 +121,7 @@ def display_page(pathname):
     else:
         return noPage
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
 
 
